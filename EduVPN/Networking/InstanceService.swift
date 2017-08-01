6e2892f346b2cddc3145573eff43970f8f7d11a2
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2017-08-01 23:06:57 +0200

    Add static and instance services.

---
+++ EduVPN/Networking/InstanceService.swift
@@ -0,0 +1,48 @@
+//
+//  InstanceService.swift
+//  EduVPN
+//
+//  Created by Jeroen Leenarts on 01-08-17.
+//  Copyright © 2017 SURFNet. All rights reserved.
+//
+
+import Foundation
+import Moya
+import Result
+
+struct DynamicTarget: TargetType {
+    let baseURL: URL
+
+    var path: String { return "/info.json" }
+    var method: Moya.Method { return .get }
+
+    var parameterEncoding: ParameterEncoding { return JSONEncoding.default }
+
+    var task: Task { return .request }
+
+    var sampleData: Data { return "".data(using: String.Encoding.utf8)! }
+
+    var parameters: [String: Any]? {
+            return nil
+    }
+}
+
+class DynamicInstanceProvider: MoyaProvider<DynamicTarget> {
+    let baseURL: URL
+
+    public init(baseURL: URL, endpointClosure: @escaping EndpointClosure = MoyaProvider.defaultEndpointMapping,
+                requestClosure: @escaping RequestClosure = MoyaProvider.defaultRequestMapping,
+                stubClosure: @escaping StubClosure = MoyaProvider.neverStub,
+                manager: Manager = MoyaProvider<DynamicTarget>.defaultAlamofireManager(),
+                plugins: [PluginType] = [],
+                trackInflights: Bool = false) {
+        self.baseURL = baseURL
+        super.init(endpointClosure: endpointClosure, requestClosure: requestClosure, stubClosure: stubClosure, manager: manager, plugins: plugins, trackInflights: trackInflights)
+
+    }
+
+    override func request(_ target: DynamicTarget, completion: @escaping Moya.Completion) -> Cancellable {
+        let dynamicTarget = DynamicTarget(baseURL: baseURL)
+        return super.request(dynamicTarget, completion: completion)
+    }
+}
---
+++ EduVPN/Networking/StaticService.swift
@@ -0,0 +1,69 @@
+//
+//  StaticService.swift
+//  EduVPN
+//
+//  Created by Jeroen Leenarts on 01-08-17.
+//  Copyright © 2017 SURFNet. All rights reserved.
+//
+
+import Foundation
+
+import Moya
+
+private let publicKey = Data(base64Encoded: "E5On0JTtyUVZmcWd+I/FXRm32nSq8R2ioyW7dcu/U88=")
+
+enum StaticService {
+    case instances
+    case instancesSignature
+    case federation
+    case federationSignature
+}
+
+extension StaticService: TargetType {
+    var baseURL: URL { return URL(string: "https://static.eduvpn.nl")! }
+
+    var path: String {
+        switch self {
+        case .instances:
+            return "/instances.json"
+        case .instancesSignature:
+            return "/instances.json.sig"
+        case .federation:
+            return "/federation.json"
+        case .federationSignature:
+            return "/federation.json.sig"
+        }
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .instances, .instancesSignature, .federation, .federationSignature:
+            return .get
+        }
+    }
+
+    var parameters: [String: Any]? {
+        switch self {
+        case .instances, .instancesSignature, .federation, .federationSignature:
+            return nil
+        }
+    }
+
+    var parameterEncoding: ParameterEncoding {
+        switch self {
+        case .instances, .instancesSignature, .federation, .federationSignature:
+            return JSONEncoding.default
+        }
+    }
+
+    var task: Task {
+        switch self {
+        case .instances, .instancesSignature, .federation, .federationSignature:
+            return .request
+        }
+    }
+
+    var sampleData: Data {
+        return "".data(using: String.Encoding.utf8)!
+    }
+}
