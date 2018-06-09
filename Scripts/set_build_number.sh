a00c8bdd3d417e8b69537b9811c6ae3683b7376b
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2018-06-09 20:18:50 +0200

    Set app version based on tag.

--- Scripts/set_build_number.sh
+++ Scripts/set_build_number.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 git=$(sh /etc/profile; which git)
+git_release_version=$("$git" describe --tags --always --abbrev=0)
 number_of_commits=$("$git" rev-list HEAD --count)
 
 target_plist="$TARGET_BUILD_DIR/$INFOPLIST_PATH"
@@ -9,6 +10,7 @@ dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
 for plist in "$target_plist" "$dsym_plist"; do
   if [ -f "$plist" ]; then
     /usr/libexec/PlistBuddy -c "Set :CFBundleVersion ${number_of_commits}" "$plist"
+    /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${git_release_version#*v}" "$plist"
   fi
 done
 
