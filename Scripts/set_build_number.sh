9d5d359a0825f72029cf18343b57d2f461ac1129
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2018-06-15 11:19:40 +0200

    Fix build versioning.

--- Scripts/set_build_number.sh
+++ Scripts/set_build_number.sh
@@ -3,6 +3,7 @@
 git=$(sh /etc/profile; which git)
 git_release_version=$("$git" describe --tags --always --abbrev=0)
 number_of_commits=$("$git" rev-list HEAD --count)
+git_release_version=${git_release_version//[v]/}
 
 target_plist="$TARGET_BUILD_DIR/$INFOPLIST_PATH"
 dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
@@ -10,14 +11,14 @@ dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
 for plist in "$target_plist" "$dsym_plist"; do
   if [ -f "$plist" ]; then
     /usr/libexec/PlistBuddy -c "Set :CFBundleVersion ${number_of_commits}" "$plist"
-    /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${git_release_version#*v}" "$plist"
+    /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${git_release_version}" "$plist"
   fi
 done
 
 settings_root_plist="$TARGET_BUILD_DIR/EduVPN.app/Settings.bundle/Root.plist"
 
 if [ -f "$settings_root_plist" ]; then
-  settingsVersion="`agvtool what-marketing-version -terse1`(${number_of_commits})"
+  settingsVersion="$git_release_version (${number_of_commits})"
   /usr/libexec/PlistBuddy -c "Set :PreferenceSpecifiers:1:DefaultValue $settingsVersion" "$settings_root_plist"
 else
   echo "Could not find: $settings_root_plist"
