d1205129a3b414ee51d417cbb127f8c2f9ce86f3
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2018-12-30 14:43:20 +0100

    Add localized message strings to system messages.

--- EduVPN/Models/Message.swift
+++ EduVPN/Models/Message.swift
@@ -41,17 +41,57 @@ extension SystemMessages {
     }
     
     var displayString: String {
-        return systemMessages.map(createString).joined(separator: "\n\n")
+        return systemMessages.compactMap{ $0.displayString }.joined(separator: "\n\n")
     }
 
 }
 
 struct Message: Decodable {
-    var message: String
+    var message: String?
+    var messages: [String: String]?
     var date_time: Date
     var type: NotificationType
 }
 
-private func createString(message: Message) -> String {
-    return [displayDateFormatter.string(from: message.date_time), message.message].joined(separator: "\n")
+extension Message {
+    enum MessageKeys: String, CodingKey {
+        case message
+        case dateTime = "date_time"
+        case type
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: MessageKeys.self)
+        
+        type = try container.decode(NotificationType.self, forKey: .type)
+        date_time = try container.decode(Date.self, forKey: .dateTime)
+        
+        // Here we try to deocde the `message` key into both a String and a [String: String]. The localizedMessage implementatation tries to obtain the "locale correct" value.
+        message = try? container.decode(String.self, forKey: .message)
+        messages = try? container.decode([String: String].self, forKey: .message)
+    }
+
+}
+
+extension Message {
+    var localizedMessage: String? {
+        if let messages = messages {
+            let preferedLocalization = Bundle.preferredLocalizations(from: Array(messages.keys))
+            for localeIdentifier in preferedLocalization {
+                if let localizedCandidate = messages[localeIdentifier] {
+                    return localizedCandidate
+                }
+            }
+        } else if let message = message {
+            return message
+        }
+        return ""
+    }
+
+    var displayString: String? {
+        guard let message = localizedMessage else {
+            return nil
+        }
+        return [displayDateFormatter.string(from: date_time), message].joined(separator: "\n")
+    }
 }
