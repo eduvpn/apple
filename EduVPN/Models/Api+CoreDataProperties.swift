5f55fa5d7aeb11ac83ce675deafb678dcf16962c
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2019-01-15 21:28:07 +0100

    Move storing of auth state and certificate to files, with data protection, excluded from backup. Fixes #49.

--- EduVPN/Models/Api+CoreDataProperties.swift
+++ EduVPN/Models/Api+CoreDataProperties.swift
@@ -51,62 +51,110 @@ extension Api {
         }
     }
 
-    private var authStateKeychainKey: String {
-        return "\(authorizationEndpoint!)|instance-info-authState"
+    private var authStateUrl: URL? {
+        guard let authStateUrl = authorizationEndpointFileUrl else { return nil }
+        return authStateUrl.appendingPathComponent("authState.bin")
+    }
+    
+    private var certificateUrl: URL? {
+        guard var certificateUrl = apiBaseFileUrl else { return nil }
+        certificateUrl.appendPathComponent("client.certificate")
+        return certificateUrl
+    }
+    
+    private var apiBaseFileUrl: URL? {
+        guard let apiBaseUri = apiBaseUri, let apiBaseUriUrl = URL(string: apiBaseUri) else { return nil }
+        return filePathUrl(from: apiBaseUriUrl)
+    }
+    
+    private var authorizationEndpointFileUrl: URL? {
+        guard let authorizationEndpoint = authorizationEndpoint, let authorizationEndpointUrl = URL(string: authorizationEndpoint) else { return nil }
+        return filePathUrl(from: authorizationEndpointUrl)
+    }
+    
+    private func filePathUrl(from url: URL) -> URL? {
+        guard var fileUrl = try? FileManager.default.url(for: .applicationSupportDirectory,
+                                                         in: .userDomainMask,
+                                                         appropriateFor: nil,
+                                                         create: false) else { return nil }
+
+        if let host = url.host {
+            fileUrl.appendPathComponent(host)
+            try? FileManager.default.createDirectory(at: fileUrl, withIntermediateDirectories: true, attributes: [FileAttributeKey.protectionKey : FileProtectionType.completeUntilFirstUserAuthentication])
+        }
+        
+        if !url.path.isEmpty {
+            fileUrl.appendPathComponent(url.path)
+            try? FileManager.default.createDirectory(at: fileUrl, withIntermediateDirectories: true, attributes: [FileAttributeKey.protectionKey : FileProtectionType.completeUntilFirstUserAuthentication])
+        }
+        
+        return fileUrl
     }
 
     var authState: OIDAuthState? {
         get {
-            if let data = KeychainSwift().getData(authStateKeychainKey) {
-                return NSKeyedUnarchiver.unarchiveObject(with: data) as? OIDAuthState
-            } else {
-                return nil
+            guard let authStateUrl = authStateUrl else { return nil }
+            if FileManager.default.fileExists(atPath: authStateUrl.path) {
+                if let data = try? Data(contentsOf: authStateUrl) {
+                    return NSKeyedUnarchiver.unarchiveObject(with: data) as? OIDAuthState
+                }
             }
+            
+            return nil
         }
         set {
+            guard let authStateUrl = authStateUrl else { return }
             if let newValue = newValue {
                 let data = NSKeyedArchiver.archivedData(withRootObject: newValue)
-                KeychainSwift().set(data, forKey: authStateKeychainKey)
+                try? data.write(to: authStateUrl, options: [.atomicWrite, .completeFileProtectionUntilFirstUserAuthentication])
+                
+                excludeFromBackup(url: authStateUrl)
             } else {
-                KeychainSwift().delete(authStateKeychainKey)
+                try? FileManager.default.removeItem(at: authStateUrl)
             }
         }
     }
-
-    private var certificateKeychainKey: String {
-        return "\(authorizationEndpoint!)|certificate"
-    }
-
+    
     var certificateModel: CertificateModel? {
         get {
-            if let data = KeychainSwift().getData(certificateKeychainKey) {
-                do {
-                    return try JSONDecoder().decode(CertificateModel.self, from: data)
-                } catch {
-                    return nil
+            guard let certificateUrl = certificateUrl else { return nil }
+            if FileManager.default.fileExists(atPath: certificateUrl.path) {
+                if let data = try? Data(contentsOf: certificateUrl) {
+                    do {
+                        return try JSONDecoder().decode(CertificateModel.self, from: data)
+                    } catch {
+                        return nil
+                    }
                 }
-            } else {
-                return nil
             }
+            
+            return nil
         }
         set {
+            guard let certificateUrl = certificateUrl else { return }
             if let oldIdentifier = certificateModel?.uniqueIdentifier {
                 if oldIdentifier != newValue?.uniqueIdentifier ?? "" {
                     UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [oldIdentifier])
                 }
             }
+
             if let newValue = newValue {
-                do {
-                    let data = try JSONEncoder().encode(newValue)
-                    KeychainSwift().set(data, forKey: certificateKeychainKey)
-                } catch {
-                }
+                let data = try? JSONEncoder().encode(newValue)
+                try? data?.write(to: certificateUrl, options: [Data.WritingOptions.atomicWrite, .completeFileProtectionUntilFirstUserAuthentication])
+
+                excludeFromBackup(url: certificateUrl)
             } else {
-                KeychainSwift().delete(certificateKeychainKey)
+                try? FileManager.default.removeItem(at: certificateUrl)
             }
         }
     }
 
+    private func excludeFromBackup(url: URL) {
+        var url = url
+        var resourceValues = URLResourceValues()
+        resourceValues.isExcludedFromBackup = true
+        try? url.setResourceValues(resourceValues)
+    }
 }
 
 // MARK: Generated accessors for profiles
--- EduVPN/Models/InstanceInfoModel.swift
+++ EduVPN/Models/InstanceInfoModel.swift
@@ -8,7 +8,6 @@
 
 import Foundation
 import AppAuth
-import KeychainSwift
 
 struct InstanceInfoModel: Decodable {
     var authorizationEndpoint: URL
