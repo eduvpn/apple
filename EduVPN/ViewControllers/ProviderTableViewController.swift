7d503655aa8117ac7bc0bba59920cb9d142af2cf
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2019-01-24 20:42:23 +0100

    Remove "add configuration" button. Fixes #59.

--- EduVPN/ViewControllers/ProviderTableViewController.swift
+++ EduVPN/ViewControllers/ProviderTableViewController.swift
@@ -39,45 +39,9 @@ class ProviderTableViewController: UITableViewController {
 
     var providerManagerCoordinator: TunnelProviderManagerCoordinator!
 
-    weak var noConfigsButton: TableTextButton?
-
-    var viewContext: NSManagedObjectContext! {
-        willSet {
-            if let context = viewContext {
-                let notificationCenter = NotificationCenter.default
-                notificationCenter.removeObserver(self, name: NSNotification.Name.NSManagedObjectContextObjectsDidChange, object: context)
-            }
-        }
-        didSet {
-            if let context = viewContext {
-                let notificationCenter = NotificationCenter.default
-                notificationCenter.addObserver(self, selector: #selector(managedObjectContextObjectsDidChange), name: NSNotification.Name.NSManagedObjectContextObjectsDidChange, object: context)
-            }
-        }
-    }
+    var viewContext: NSManagedObjectContext!
 
     var providerType: ProviderType = .unknown
-
-    @objc
-    func managedObjectContextObjectsDidChange(notification: NSNotification) {
-        DispatchQueue.main.asyncAfter(deadline: .now() + .nanoseconds(1)) { [weak self] in
-            self?.checkButton()
-        }
-        
-    }
-    
-    private func checkButton() {
-        if self.fetchedResultsController.count == 0 && noConfigsButton == nil && providerType == .unknown {
-            let noConfigsButton = TableTextButton()
-            noConfigsButton.title = NSLocalizedString("Add configuration", comment: "")
-            noConfigsButton.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-            noConfigsButton.frame = tableView.bounds
-            tableView.tableHeaderView = noConfigsButton
-            self.noConfigsButton = noConfigsButton
-        } else if self.fetchedResultsController.count > 0 {
-            tableView.tableHeaderView = nil
-        }
-    }
     
     private lazy var fetchedResultsController: FetchedResultsController<Instance> = {
         let fetchRequest = NSFetchRequest<Instance>()
@@ -118,22 +82,11 @@ class ProviderTableViewController: UITableViewController {
             block()
         }
     }
-
-    @objc
-    func tableTextButtonTapped() {
-        if let _ = Config.shared.predefinedProvider {
-            delegate?.addPredefinedProvider(providerTableViewController: self)
-        } else {
-            delegate?.addProvider(providerTableViewController: self)
-        }
-    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         refresh()
-        
-        checkButton()
     }
     
     func refresh() {
@@ -398,38 +351,5 @@ class InstanceFetchedResultsControllerDelegate: NSObject, FetchedResultsControll
         case let .delete(_, index):
             tableView.deleteSections(IndexSet(integer: index), with: .automatic)
         }
-    }
-}
-
-class TableTextButton: UIView {
-    let button: UIButton
-    
-    var title: String? {
-        get {
-            return button.title(for: .normal)
-            
-        }
-        set(value) {
-            button.setTitle(value, for: .normal)
-        }
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("not been implemented")
-    }
-    
-    init() {
-        button = UIButton(type: .system)
-        button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
-        button.titleLabel?.adjustsFontForContentSizeCategory = true
-        super.init(frame: CGRect.zero)
-        addSubview(button)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            button.centerXAnchor.constraint(equalTo: self.centerXAnchor),
-            button.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            NSLayoutConstraint(item: button, attribute: .width, relatedBy: .equal, toItem: button, attribute: .width, multiplier: 1, constant: 250)
-            ])
-        button.addTarget(nil, action: #selector(ProviderTableViewController.tableTextButtonTapped), for: .touchUpInside)
     }
 }
