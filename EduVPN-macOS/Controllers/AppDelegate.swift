d4c07cdc05d208c6d17d6e6288c36f3f7b6f6d3a
Aleksandr Poddubny <poddubny@quality-wolves.com>
2019-05-16 15:30:51 +0300

    Created basic targets for eduVPN and Let's Connect macOS apps

---
+++ EduVPN-macOS/Controllers/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EduVPN-macOS
+//
+//  Created by Aleksandr Poddubny on 16/05/2019.
+//  Copyright © 2019 SURFNet. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EduVPN-macOS/Models/AppConfigType.swift
@@ -0,0 +1,15 @@
+//
+//  AppConfigType.swift
+//  eduVPN
+//
+//  Created by Johan Kool on 10/09/2018.
+//  Copyright © 2017-2019 Commons Conservancy.
+//
+
+protocol AppConfigType {
+    
+    var appName: String { get }
+    var clientId: String { get }
+    
+    var apiDiscoveryEnabled: Bool { get }
+}
