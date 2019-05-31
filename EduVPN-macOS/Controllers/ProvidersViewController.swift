1a1a87ae888e0388a0de0770a8cbe1476b1b1693
Aleksandr Poddubny <poddubny@quality-wolves.com>
2019-05-31 13:26:04 +0300

    Moved deselecting table view to separate file

--- EduVPN-macOS/Controllers/ProvidersViewController.swift
+++ EduVPN-macOS/Controllers/ProvidersViewController.swift
@@ -460,25 +460,3 @@ extension ProvidersViewController: NSTableViewDelegate {
         }
     }
 }
-
-class DeselectingTableView: NSTableView {
-    
-    override open func mouseDown(with event: NSEvent) {
-        let beforeIndex = selectedRow
-        
-        super.mouseDown(with: event)
-        
-        let point = convert(event.locationInWindow, from: nil)
-        let rowIndex = row(at: point)
-        
-        if rowIndex < 0 {
-            deselectAll(nil)
-        } else if rowIndex == beforeIndex {
-            deselectRow(rowIndex)
-        } else if let delegate = delegate {
-            if !delegate.tableView!(self, shouldSelectRow: rowIndex) {
-                deselectAll(nil)
-            }
-        }
-    }
-}
---
+++ EduVPN-macOS/Views/DeselectingTableView.swift
@@ -0,0 +1,31 @@
+//
+//  DeselectingTableView.swift
+//  EduVPN-macOS
+//
+//  Created by Aleksandr Poddubny on 31/05/2019.
+//  Copyright © 2019 SURFNet. All rights reserved.
+//
+
+import Cocoa
+
+class DeselectingTableView: NSTableView {
+    
+    override open func mouseDown(with event: NSEvent) {
+        let beforeIndex = selectedRow
+        
+        super.mouseDown(with: event)
+        
+        let point = convert(event.locationInWindow, from: nil)
+        let rowIndex = row(at: point)
+        
+        if rowIndex < 0 {
+            deselectAll(nil)
+        } else if rowIndex == beforeIndex {
+            deselectRow(rowIndex)
+        } else if let delegate = delegate {
+            if !delegate.tableView!(self, shouldSelectRow: rowIndex) {
+                deselectAll(nil)
+            }
+        }
+    }
+}
