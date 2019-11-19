efb89fe6459144480e78e58b629611cfee8e1bd9
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2019-11-19 22:26:26 +0100

    Improved fix using standardized API and add unit test for iOS target.

--- EduVPN-multiOS/Services/FileService.swift
+++ EduVPN-multiOS/Services/FileService.swift
@@ -8,6 +8,36 @@
 
 import Foundation
 
+func filePathUrl(from url: URL) -> URL? {
+    guard var fileUrl = applicationSupportDirectoryUrl() else { return nil }
+
+    if let host = url.host {
+        fileUrl.appendPathComponent(host)
+    }
+
+    if !url.path.isEmpty {
+        fileUrl.appendPathComponent(url.path)
+    }
+
+    fileUrl = fileUrl.standardizedFileURL
+
+    do {
+        #if os(iOS)
+        let attributes: [FileAttributeKey: Any]? = [FileAttributeKey.protectionKey: FileProtectionType.completeUntilFirstUserAuthentication]
+        #elseif os(macOS)
+        let attributes: [FileAttributeKey: Any]? = nil
+        #endif
+
+        try FileManager.default.createDirectory(at: fileUrl,
+                                                withIntermediateDirectories: true,
+                                                attributes: attributes)
+    } catch {
+        return nil
+    }
+
+    return fileUrl
+}
+
 func applicationSupportDirectoryUrl() -> URL? {
     guard var url = try? FileManager.default.url(for: .applicationSupportDirectory,
                                                  in: .userDomainMask,
