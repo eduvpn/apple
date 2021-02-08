981d0550abe7e8bc48c0ade05580f77fa7ae7435
Roopesh Chander <roop@roopc.net>
2021-02-08 16:41:33 +0530

    PacketTunnelProvider: Handle preventing automatic connections

--- EduVPNTunnelExtension-macOS/PacketTunnelProvider.swift
+++ EduVPNTunnelExtension-macOS/PacketTunnelProvider.swift
@@ -4,6 +4,37 @@
 //
 
 import TunnelKit
+import NetworkExtension
+
+enum PacketTunnelProviderError: Error {
+    case connectionAttemptFromOSNotAllowed
+}
 
 class PacketTunnelProvider: OpenVPNTunnelProvider {
+    override var reasserting: Bool {
+        didSet {
+            if reasserting {
+                if let tunnelProtocol = protocolConfiguration as? NETunnelProviderProtocol {
+                    if tunnelProtocol.shouldPreventAutomaticConnections {
+                        stopTunnel(with: .none, completionHandler: {})
+                    }
+                }
+            }
+        }
+    }
+
+    override func startTunnel(options: [String: NSObject]? = nil, completionHandler: @escaping (Error?) -> Void) {
+        let startTunnelOptions = StartTunnelOptions(options: options ?? [:])
+        if !startTunnelOptions.isStartedByApp {
+            if let tunnelProtocol = protocolConfiguration as? NETunnelProviderProtocol {
+                if tunnelProtocol.shouldPreventAutomaticConnections {
+                    Darwin.sleep(3) // Prevent rapid connect-disconnect cycles
+                    completionHandler(PacketTunnelProviderError.connectionAttemptFromOSNotAllowed)
+                    return
+                }
+            }
+        }
+
+        super.startTunnel(options: options, completionHandler: completionHandler)
+    }
 }
