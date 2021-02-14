dccb2d9b1b13f948edf7c8fca71fac44df552956
Roopesh Chander <roop@roopc.net>
2021-02-15 00:55:11 +0530

    macOS: Introducing AddServerViewController

---
+++ EduVPN/Controllers/AddServerViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AddServerViewController.swift
+//  EduVPN
+//
+
+#if os(macOS)
+import AppKit
+#endif
+
+protocol AddServerViewControllerDelegate: class {
+    func addServerViewController(
+        _ controller: AddServerViewController,
+        addedSimpleServerWithBaseURL baseURLString: DiscoveryData.BaseURLString,
+        authState: AuthState)
+}
+
+final class AddServerViewController: ViewController, ParametrizedViewController {
+    struct Parameters {
+        let environment: Environment
+        let preDefinedBaseURLString: DiscoveryData.BaseURLString?
+    }
+
+    weak var delegate: AddServerViewControllerDelegate?
+
+    private var parameters: Parameters!
+
+    func initializeParameters(_ parameters: Parameters) {
+        guard self.parameters == nil else {
+            fatalError("Can't initialize parameters twice")
+        }
+        self.parameters = parameters
+    }
+}
