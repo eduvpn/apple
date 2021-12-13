6ff6983b41d0eb4736c03f62b6d6945c32ed2d5e
Roopesh Chander <roop@roopc.net>
2021-12-13 13:39:34 +0530

    Introducing LoggingService

---
+++ EduVPN/Services/LoggingService.swift
@@ -0,0 +1,94 @@
+//
+//  LoggingService.swift
+//  EduVPN
+//
+
+import Foundation
+import NetworkExtension
+import PromiseKit
+
+class LoggingService {
+
+    private let logFileURL: URL? = {
+        let appGroup: String = {
+            let appBundleId = Bundle.main.bundleIdentifier ?? ""
+            #if os(macOS)
+            return "\((Bundle.main.infoDictionary?["AppIdentifierPrefix"] as? String) ?? "")group.\(appBundleId)"
+            #elseif os(iOS)
+            return "group.\(appBundleId)"
+            #endif
+        }()
+        let appGroupURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: appGroup)
+        return appGroupURL?.appendingPathComponent("debug.log")
+    }()
+    private let maxLogLines = 1000
+
+    private var logLines: [String]
+    private let dateFormatter: DateFormatter
+    private var logSeparator = "--- EOF ---"
+    private var appLogStarter = "App:"
+    private var tunnelLogStarter = "Tunnel:"
+
+    private let connectionService: ConnectionService
+
+    init(connectionService: ConnectionService) {
+        self.connectionService = connectionService
+        self.logLines = []
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+        self.dateFormatter = dateFormatter
+    }
+
+    func appLog(_ message: String) {
+        let timestamp = dateFormatter.string(from: Date())
+        let line = "\(timestamp) \(message)"
+        NSLog("\(line)\n")
+        if logLines.count >= maxLogLines {
+            logLines.removeFirst()
+        }
+        if logLines.isEmpty {
+            logLines.append(appLogStarter)
+        }
+        logLines.append(line)
+    }
+
+    func flushLogToDisk() {
+        appLog("Flushing app log to disk ...")
+
+        if let url = logFileURL {
+            let content = logLines.joined(separator: "\n")
+            let data = content.data(using: .utf8)
+            if FileManager.default.fileExists(atPath: url.path) {
+                do {
+                    let fileHandle = try FileHandle(forWritingTo: url)
+                    if let data = data {
+                        fileHandle.seekToEndOfFile()
+                        if let newLines = "\n\n".data(using: .utf8) {
+                            fileHandle.write(newLines)
+                        }
+                        fileHandle.write(data)
+                        fileHandle.closeFile()
+                    }
+                } catch {
+                    NSLog("Error appending app log to disk: \(error)")
+                }
+            } else {
+                do {
+                    try content.write(to: url, atomically: true, encoding: .utf8)
+                } catch {
+                    NSLog("Error creating app log on disk: \(error)")
+                }
+            }
+            logLines = []
+        }
+    }
+
+    func getLog() -> Promise<String?> {
+        firstly {
+            connectionService.getConnectionLog()
+        }.map { [weak self] logContent in
+            guard let self = self else { return nil }
+            return (logContent ?? "") + self.logLines.joined(separator: "\n")
+        }
+    }
+}
