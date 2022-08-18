1e8a80a1549b386ff90911d2c90006884e1dcdbc
Roopesh Chander <roop@roopc.net>
2022-08-18 08:54:17 +0530

    macOS: Make importing AppAuth Obj-C code work

--- EduVPN/Helpers/Mac/OAuthRedirectHTTPHandler.h
+++ EduVPN/Helpers/Mac/OAuthRedirectHTTPHandler.h
@@ -6,10 +6,14 @@
 #ifndef OAuthRedirectHTTPHandler_h
 #define OAuthRedirectHTTPHandler_h
 
+#ifdef TARGET_OS_OSX
+
 #import <Foundation/Foundation.h>
-#import <AppAuth/AppAuth.h>
+#import "macOS/OIDRedirectHTTPHandler.h"
 
 @interface OAuthRedirectHTTPHandler: OIDRedirectHTTPHandler
 @end
+
+#endif
 
 #endif /* OAuthRedirectHTTPHandler_h */
