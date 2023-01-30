198d68c94f91c7554e497cc942a4ef6181a5e522
Roopesh Chander <roop@roopc.net>
2023-01-30 11:24:20 +0530

    Scripts: Fix create_eduvpn_installer_macos.sh

--- Scripts/create_eduvpn_installer_macos.sh
+++ Scripts/create_eduvpn_installer_macos.sh
@@ -16,12 +16,12 @@ APP_VERSION="3.0.6"
 MIN_MACOS_VERSION="10.15.0"
 
 EDUVPN_APP_NAME="eduVPN"
-EDUVPN_APP_ID="org.eduvpn.app"
+EDUVPN_APP_ID="org.eduvpn.app.mac-dev-id"
 EDUVPN_DEVELOPMENT_TEAM="ZYJ4TZX4UU"
 EDUVPN_INSTALLER_CERTIFICATE_CN="Developer ID Installer: SURF B.V. (ZYJ4TZX4UU)"
 
-LETSCONNECT_APP_NAME="Let’s Connect!"
-LETSCONNECT_APP_ID="com.commonscaretakers.letsconnect.mac"
+LETSCONNECT_APP_NAME='Let’s Connect!'
+LETSCONNECT_APP_ID="com.commonscaretakers.letsconnect.mac-dev-id"
 LETSCONNECT_DEVELOPMENT_TEAM="D9T87NF4Q7"
 LETSCONNECT_INSTALLER_CERTIFICATE_CN="Developer ID Installer: Commons Caretakers b.v. (D9T87NF4Q7)"
 
@@ -70,45 +70,80 @@ echo "  APP_ID = ${APP_ID}"
 echo "  DEVELOPMENT_TEAM = ${DEVELOPMENT_TEAM}"
 echo "  CERTIFICATE_CN=${CERTIFICATE_CN}"
 
+
+APP_FILENAME="${APP_NAME}.app"
+
 echo ""
-echo "Checking app..."
-if [ -f "${APP_NAME}.app" ]; then
-    spctl -vvv --assess -t exec "${APP_NAME}.app" 2>&1 | tee /tmp/spctl_app.log
+echo -n "-> Checking app \"${APP_FILENAME}\"..."
+date "+ (at %H:%M:%S)"
+
+if [ -e "${APP_FILENAME}" ]; then
+    spctl -vvv --assess -t exec "${APP_FILENAME}" 2>&1 | tee /tmp/spctl_app.log
     if grep -q accepted /tmp/spctl_app.log; then
-	echo "App \"${APP_NAME}.app\" appears to be notarized."
+	echo "App \"${APP_FILENAME}\" appears to be notarized."
     else
-        echo "Error: App \"${APP_NAME}.app\" is not notarized. Not creating installer." 1>&2; exit 1;
+        echo "Error: App \"${APP_FILENAME}\" is not notarized. Not creating installer." 1>&2; exit 1;
     fi
     rm -rf /tmp/spctl_app.log
 else
-    echo "Error: App \"${APP_NAME}.app\" not found. Not creating installer." 1>&2; exit 1;
+    echo "Error: App \"${APP_FILENAME}\" not found. Not creating installer." 1>&2; exit 1;
+fi
+
+PACKAGE_FILENAME="./${n}_${APP_VERSION}.pkg"
+echo ""
+echo -n "-> Creating installer package \"${PACKAGE_FILENAME}\"..."
+date "+ (at %H:%M:%S)"
+
+pkgbuild --root "${APP_FILENAME}" --identifier ${APP_ID} --version ${APP_VERSION} --install-location "/Applications/${APP_NAME}.app" --min-os-version ${MIN_MACOS_VERSION} --sign "${CERTIFICATE_CN}" ${PACKAGE_FILENAME}
+
+if [ $? -ne 0 ]; then
+    exit 1
 fi
 
 echo ""
-echo "Creating installer package..."
-pkgbuild --root "\"${APP_NAME}.app\"" --identifier ${APP_ID} --version ${APP_VERSION} --install-location "\"/Applications/${APP_NAME}.app\"" --min-os-version ${MIN_MACOS_VERSION} --sign "\"${CERTIFICATE_CN}\"" ./${n}_${APP_VERSION}.pkg
+echo -n "-> Notarizing installer package \"${PACKAGE_FILENAME}\"..."
+date "+ (at %H:%M:%S)"
+
+xcrun notarytool submit ${PACKAGE_FILENAME} --apple-id "${u}" --password "${p}" --team-id ${DEVELOPMENT_TEAM} --wait
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 
 echo ""
-echo "Notarizing installer package..."
-xcrun notarytool submit ./${n}_${APP_VERSION}.pkg --apple-id "${u}" --password "${p}" --team-id ${DEVELOPMENT_TEAM} --wait
+echo -n "-> Adding notarization information to package \"${PACKAGE_FILENAME}\"..."
+date "+ (at %H:%M:%S)"
+
+xcrun stapler staple ${PACKAGE_FILENAME}
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 
 echo ""
-echo "Adding notarization information to package..."
-xcrun stapler staple ./${n}_${APP_VERSION}.pkg
+echo -n "-> Checking installer package \"${PACKAGE_FILENAME}\"..."
+date "+ (at %H:%M:%S)"
 
-echo "Checking installer package..."
-if [ -f "./${n}_${APP_VERSION}.pkg" ]; then
-    spctl -vvv --assess -t install "./${n}_${APP_VERSION}.pkg" 2>&1 | tee /tmp/spctl_installer.log
+if [ -e "${PACKAGE_FILENAME}" ]; then
+    spctl -vvv --assess -t install "${PACKAGE_FILENAME}" 2>&1 | tee /tmp/spctl_installer.log
     if grep -q accepted /tmp/spctl_installer.log; then
 	echo "Looks good."
     else
-        echo "Error: \"./${n}_${APP_VERSION}.pkg\" is not notarized." 1>&2; exit 1;
+        echo "Error: \"${PACKAGE_FILENAME}\" is not notarized." 1>&2; exit 1;
     fi
     rm -rf /tmp/spctl_installer.log
 else
-    echo "Error: \"./${n}_${APP_VERSION}.pkg\" is not found." 1>&2; exit 1;
+    echo "Error: \"${PACKAGE_FILENAME}\" is not found." 1>&2; exit 1;
+fi
+
+if [ $? -ne 0 ]; then
+    exit 1
 fi
 
 echo ""
-echo "Notarized installation package is at: ./${n}_${APP_VERSION}.pkg"
+echo -n "-> Done"
+date "+ (at %H:%M:%S)"
+
+echo ""
+echo "Notarized installation package is at: \"${PACKAGE_FILENAME}\""
 
