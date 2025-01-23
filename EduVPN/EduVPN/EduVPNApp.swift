50792e5837f7ed1e269d417d5fa8db684677272f
Roopesh Chander <roop@roopc.net>
2025-01-23 12:15:58 +0530

    Create new multiplatform app in Xcode
    Edit the project file to use values from xcconfig

---
+++ EduVPN/EduVPN/EduVPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduVPNApp.swift
+//  EduVPN
+//
+//  Created by Roopesh Chander S on 23/01/25.
+//
+
+import SwiftUI
+
+@main
+struct EduVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
